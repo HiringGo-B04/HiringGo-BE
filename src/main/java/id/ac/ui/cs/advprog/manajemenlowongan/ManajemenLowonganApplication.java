c63e742773e781525e4296c445961404c7c3fc1b
rakbidb <rakbidb@gmail.com>
2025-03-28 20:09:27 +0700

    inisialisasi project

---
+++ src/main/java/id/ac/ui/cs/advprog/manajemenlowongan/ManajemenLowonganApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.manajemenlowongan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManajemenLowonganApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManajemenLowonganApplication.class, args);
+	}
+
+}
