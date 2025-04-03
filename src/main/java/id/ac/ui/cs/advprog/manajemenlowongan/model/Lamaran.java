193c4a32a5005256ca23c608eff6d8a938efcbc1
rakbidb <rakbidb@gmail.com>
2025-04-03 23:53:29 +0700

    [ADD]: added Lamaran.java

---
+++ src/main/java/id/ac/ui/cs/advprog/manajemenlowongan/model/Lamaran.java
@@ -0,0 +1,23 @@
+package id.ac.ui.cs.advprog.manajemenlowongan.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+public class Lamaran {
+    private UUID id;
+    private UUID idMahasiswa;
+    private UUID idLowongan;
+    private int sks;
+    private float gpa;
+    private StatusLamaran status;
+
+    public void updateStatus(StatusLamaran status) {
+        this.status = status;
+    }
+}
+
+enum StatusLamaran {
+    MENUNGGU, DITERIMA, DITOLAK;
+}
