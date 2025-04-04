5980cbdc93c7c607df1f45f7c7f713bf2ad8dd9a
rakbidb <rakbidb@gmail.com>
2025-04-04 09:28:04 +0700

    [➕ADD]: added LowonganService.java

---
+++ src/main/java/id/ac/ui/cs/advprog/manajemenlowongan/service/LowonganService.java
@@ -0,0 +1,16 @@
+package id.ac.ui.cs.advprog.manajemenlowongan.service;
+
+import id.ac.ui.cs.advprog.manajemenlowongan.model.*;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface LowonganService {
+    public List<Lowongan> findAll();
+    public Lowongan addLowongan(Lowongan lowongan);
+    public Lowongan updateLowongan(Lowongan lowongan);
+    public Lowongan deleteLowongan(Lowongan lowongan);
+    public Lowongan findLowonganById(UUID id);
+    public Lamaran acceptLamaran(UUID idLamaran);
+    public Lamaran rejectLamaran(UUID idLamaran);
+}
