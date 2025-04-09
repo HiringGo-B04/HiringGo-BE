df7f025c2f81eafc141d7e77b2ddc586b480eb53
rakbidb <rakbidb@gmail.com>
2025-04-09 07:55:17 +0700

    [🎨DESIGN PATTERN]: implement Builder Pattern on Lowongan

--- src/main/java/id/ac/ui/cs/advprog/manajemenlowongan/model/Lowongan.java
+++ src/main/java/id/ac/ui/cs/advprog/manajemenlowongan/model/Lowongan.java
@@ -14,4 +14,60 @@ public class Lowongan {
     private int totalAsdosNeeded;
     private int totalAsdosRegistered;
     private int totalAsdosAccepted;
+
+    private Lowongan() {
+        // private constructor to force usage of builder
+    }
+
+    public static class Builder {
+        private UUID id = UUID.randomUUID(); // default to new ID
+        private String matkul;
+        private int year;
+        private String term;
+        private int totalAsdosNeeded;
+        private int totalAsdosRegistered = 0;
+        private int totalAsdosAccepted = 0;
+
+        public Builder matkul(String matkul) {
+            this.matkul = matkul;
+            return this;
+        }
+
+        public Builder year(int year) {
+            this.year = year;
+            return this;
+        }
+
+        public Builder term(String term) {
+            this.term = term;
+            return this;
+        }
+
+        public Builder totalAsdosNeeded(int totalAsdosNeeded) {
+            this.totalAsdosNeeded = totalAsdosNeeded;
+            return this;
+        }
+
+        public Builder totalAsdosRegistered(int totalAsdosRegistered) {
+            this.totalAsdosRegistered = totalAsdosRegistered;
+            return this;
+        }
+
+        public Builder totalAsdosAccepted(int totalAsdosAccepted) {
+            this.totalAsdosAccepted = totalAsdosAccepted;
+            return this;
+        }
+
+        public Lowongan build() {
+            Lowongan lowongan = new Lowongan();
+            lowongan.id = this.id;
+            lowongan.matkul = this.matkul;
+            lowongan.year = this.year;
+            lowongan.term = this.term;
+            lowongan.totalAsdosNeeded = this.totalAsdosNeeded;
+            lowongan.totalAsdosRegistered = this.totalAsdosRegistered;
+            lowongan.totalAsdosAccepted = this.totalAsdosAccepted;
+            return lowongan;
+        }
+    }
 }
