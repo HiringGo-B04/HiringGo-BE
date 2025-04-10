937f8752ca9426613b84eb046fa3662f91a311f6
DaoistXuandu <akbarraihan2005@gmail.com>
2025-04-11 01:51:38 +0700

    [GREEN] impl logout service

--- src/main/java/id/ac/ui/cs/advprog/authjwt/service/AuthService.java
+++ src/main/java/id/ac/ui/cs/advprog/authjwt/service/AuthService.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.HashMap;
@@ -65,8 +66,30 @@ public class AuthService implements AuthenticationFacade {
         }
     }
 
+    @Transactional
     @Override
-    public ResponseEntity<Map<String, String>>  register(@RequestBody User user) {
+    public ResponseEntity<Map<String, String>> logout(Token token){
+        Map<String, String> response = new HashMap<>();
+        try{
+            if(tokenRepository.findByToken(token.getToken()) == null) {
+                throw new IllegalArgumentException("Invalid Token");
+            }
+
+            tokenRepository.deleteByToken(token.getToken());
+
+            response.put("status", "accept");
+            response.put("messages", "Success to logout");
+            return new ResponseEntity<>(response, HttpStatus.valueOf(200));
+        }
+        catch (Exception e) {
+            response.put("status", "error");
+            response.put("messages", e.getMessage());
+            return new ResponseEntity<>(response, HttpStatus.valueOf(401));
+        }
+    }
+
+    @Override
+    public ResponseEntity<Map<String, String>> register(@RequestBody User user) {
         Map<String, String> response = new HashMap<>();
         if (userRepository.existsByUsername(user.getUsername())) {
             response.put("status", "error");
