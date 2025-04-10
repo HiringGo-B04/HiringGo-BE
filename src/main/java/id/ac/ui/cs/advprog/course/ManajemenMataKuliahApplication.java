200390a8aed47341f9b264613e4bc0a42df4c81c
KoecengDaun <Laurentius.farel@gmail.com>
2025-04-10 20:31:43 +0700

    initializing new project

---
+++ src/main/java/id/ac/ui/cs/advprog/course/ManajemenMataKuliahApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManajemenMataKuliahApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManajemenMataKuliahApplication.class, args);
+    }
+
+}
