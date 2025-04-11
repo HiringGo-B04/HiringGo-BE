da42f77cccff9b3aa2a6858236693b699c01b3db
AlyssaLayla <alyssasasti@gmail.com>
2025-04-11 19:46:30 +0700

    [RED] Add Skeleton ApproveLogCommand.java

---
+++ src/main/java/id/ac/ui/cs/advprog/log/service/command/ApproveLogCommand.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.log.service.command;
+
+import id.ac.ui.cs.advprog.log.model.Log;
+
+public class ApproveLogCommand implements LogCommand {
+
+    public ApproveLogCommand(Log log) {
+    }
+
+    @Override
+    public void execute() {
+    }
+}
