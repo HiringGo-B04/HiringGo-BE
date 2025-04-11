335b1ffc6395acdc3c1dd910ea0a29369d50f1e4
AlyssaLayla <alyssasasti@gmail.com>
2025-04-11 18:52:49 +0700

    [GREEN] Implement Log Model

--- src/main/java/id/ac/ui/cs/advprog/log/model/Log.java
+++ src/main/java/id/ac/ui/cs/advprog/log/model/Log.java
@@ -16,50 +16,66 @@ public class Log {
 
     // Getters & Setters
     public String getJudul() {
+        return judul;
     }
 
     public void setJudul(String judul) {
+        this.judul = judul;
     }
 
     public String getKeterangan() {
+        return keterangan;
     }
 
     public void setKeterangan(String keterangan) {
+        this.keterangan = keterangan;
     }
 
     public String getKategori() {
+        return kategori;
     }
 
     public void setKategori(String kategori) {
+        this.kategori = kategori;
     }
 
     public LocalDate getTanggalLog() {
+        return tanggalLog;
     }
 
     public void setTanggalLog(LocalDate tanggalLog) {
+        this.tanggalLog = tanggalLog;
     }
 
     public LocalTime getWaktuMulai() {
+        return waktuMulai;
     }
 
     public void setWaktuMulai(LocalTime waktuMulai) {
+        this.waktuMulai = waktuMulai;
     }
 
     public LocalTime getWaktuSelesai() {
+        return waktuSelesai;
     }
 
     public void setWaktuSelesai(LocalTime waktuSelesai) {
+        this.waktuSelesai = waktuSelesai;
     }
 
     public String getPesanUntukDosen() {
+        return pesanUntukDosen;
     }
 
     public void setPesanUntukDosen(String pesanUntukDosen) {
+        this.pesanUntukDosen = pesanUntukDosen;
     }
 
     public String getStatus() {
+        return status;
     }
 
     public void setStatus(String status) {
+        this.status = status;
     }
 }
