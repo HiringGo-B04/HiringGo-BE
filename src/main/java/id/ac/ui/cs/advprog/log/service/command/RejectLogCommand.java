8d472a7bc2f77c3fe348be003d432166d8d0a8e2
AlyssaLayla <alyssasasti@gmail.com>
2025-04-11 19:57:47 +0700

    [RED] Add Skeleton RejectLogCommand.java

---
+++ src/main/java/id/ac/ui/cs/advprog/log/service/command/RejectLogCommand.java
@@ -0,0 +1,16 @@
+package id.ac.ui.cs.advprog.log.service.command;
+
+import id.ac.ui.cs.advprog.log.model.Log;
+
+public class RejectLogCommand implements LogCommand {
+
+
+    public RejectLogCommand(Log log) {
+
+    }
+
+    @Override
+    public void execute() {
+
+    }
+}
