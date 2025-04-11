a29eb624bcf7dce68df4e1b80ece4f37589366e0
KoecengDaun <Laurentius.farel@gmail.com>
2025-04-11 16:10:24 +0700

    Memperbaiki Test untuk CI

--- src/test/java/id/ac/ui/cs/advprog/course/ManajemenMataKuliahApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/course/ManajemenMataKuliahApplicationTests.java
@@ -1,13 +1,24 @@
 package id.ac.ui.cs.advprog.course;
 
+import id.ac.ui.cs.advprog.course.service.MataKuliahService;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootTest
+@SpringBootTest(classes = {ManajemenMataKuliahApplication.class, ManajemenMataKuliahApplicationTests.TestConfig.class})
 class ManajemenMataKuliahApplicationTests {
+
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        public MataKuliahService mataKuliahService() {
+            return Mockito.mock(MataKuliahService.class);
+        }
+    }
 
     @Test
     void contextLoads() {
     }
-
 }
