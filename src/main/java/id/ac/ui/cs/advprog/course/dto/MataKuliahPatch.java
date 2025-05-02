7228577c0a2ea2ea1950bb85de48975d14703277
KoecengDaun <laurentius.farel@gmail.com>
2025-05-02 22:29:42 +0700

    [RED] feat(dto): add MataKuliahPatch for partial updates

---
+++ src/main/java/id/ac/ui/cs/advprog/course/dto/MataKuliahPatch.java
@@ -0,0 +1,9 @@
+package id.ac.ui.cs.advprog.course.dto;
+
+import jakarta.validation.constraints.Min;
+
+public record MataKuliahPatch(
+        @Min(0) Integer sks,
+        String deskripsi
+
+) { }
