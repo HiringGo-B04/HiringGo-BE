8f844ae9f096754373a718dc949c73928fc76c52
rakbidb <rakbidb@gmail.com>
2025-05-04 16:55:08 +0700

    [RED]: fixed added LowonganServiceImpl skeleton

--- src/main/java/id/ac/ui/cs/advprog/manajemenlowongan/service/LowonganServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/manajemenlowongan/service/LowonganServiceImpl.java
@@ -1,4 +1,51 @@
 package id.ac.ui.cs.advprog.manajemenlowongan.service;
 
-public class LowonganServiceImpl {
+import id.ac.ui.cs.advprog.manajemenlowongan.model.Lowongan;
+import id.ac.ui.cs.advprog.manajemenlowongan.repository.LowonganRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class LowonganServiceImpl implements LowonganService {
+    
+    private final LowonganRepository lowonganRepository;
+
+    public LowonganServiceImpl(LowonganRepository lowonganRepository) {
+        this.lowonganRepository = lowonganRepository;
+    }
+
+    @Override
+    public List<Lowongan> getLowongan() {
+        return null;
+    }
+
+    @Override
+    public Lowongan getLowonganById(UUID id) {
+        return null;
+    }
+
+    @Override
+    public Lowongan addLowongan(Lowongan lowongan) {
+        return null;
+    }
+
+    @Override
+    public Lowongan updateLowongan(UUID id, Lowongan lowongan) {
+        return null;
+    }
+
+    public void deleteLowongan(UUID id) {}
+
+    @Override
+    public boolean isLowonganExists(Lowongan lowongan) {
+        return false;
+    }
+
+    @Override
+    public boolean isLowonganClosed(Lowongan lowongan) {
+        return false;
+    }
 }
