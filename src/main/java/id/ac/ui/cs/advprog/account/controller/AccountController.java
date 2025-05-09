2850ce6763b903a53639c93da4a726b5fe185996
DaoistXuandu <akbarraihan2005@gmail.com>
2025-05-09 15:11:39 +0700

    [GREEN] create impl for delete request dto

--- src/main/java/id/ac/ui/cs/advprog/account/controller/AccountController.java
+++ src/main/java/id/ac/ui/cs/advprog/account/controller/AccountController.java
@@ -1,20 +1,23 @@
-package id.ac.ui.cs.advprog.account.controller;
-import id.ac.ui.cs.advprog.account.service.AccountService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RestController;
-import java.util.Map;
-import id.ac.ui.cs.advprog.authjwt.model.User;
-
-@RestController
-@RequestMapping("/api/account")
-public class AccountController {
-    @Autowired
-    private AccountService accountService;
-
-    @PostMapping("/admin/delete")
-    public ResponseEntity<Map<String, String>> delete(@RequestBody User user) {
-        return accountService.delete(user.getUsername());
-    }
-}
+//package id.ac.ui.cs.advprog.account.controller;
+//import id.ac.ui.cs.advprog.account.service.AccountService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.*;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.RestController;
+//import java.util.Map;
+//import id.ac.ui.cs.advprog.authjwt.model.User;
+//
+//@RestController
+//@RequestMapping("/api/account")
+//public class AccountController {
+//    private final AccountService accountService;
+//
+//    public AccountController(AccountService accountService) {
+//        this.accountService = accountService;
+//    }
+//
+//    @PostMapping("/admin/delete")
+//    public ResponseEntity<Map<String, String>> delete(@RequestBody User user) {
+//        return accountService.delete(user.getUsername());
+//    }
+//}
