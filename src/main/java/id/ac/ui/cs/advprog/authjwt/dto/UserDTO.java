cee8a8c5bd73c093c70d8c3ac777264dd4b6c9ae
DaoistXuandu <akbarraihan2005@gmail.com>
2025-05-09 09:25:34 +0700

    [GREEN] add impl for user dto

--- src/main/java/id/ac/ui/cs/advprog/authjwt/dto/UserDTO.java
+++ src/main/java/id/ac/ui/cs/advprog/authjwt/dto/UserDTO.java
@@ -1,20 +1,7 @@
 package id.ac.ui.cs.advprog.authjwt.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+public interface UserDTO {
+        String username();
+        String password();
+}
 
-public record LecturerDto(
-        @NotBlank
-        String username,
-
-        @NotBlank
-        String password,
-
-        @NotNull
-        @NotBlank
-        String nip,
-
-        @NotNull
-        @NotBlank
-        String fullName
-) {}
