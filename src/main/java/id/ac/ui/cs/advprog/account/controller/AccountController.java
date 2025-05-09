2ff3fceba91c56833860974a96b28b6c5e61001f
DaoistXuandu <akbarraihan2005@gmail.com>
2025-05-09 15:40:55 +0700

    [GREEN] add impl for account controller delete

--- src/main/java/id/ac/ui/cs/advprog/account/controller/AccountController.java
+++ src/main/java/id/ac/ui/cs/advprog/account/controller/AccountController.java
@@ -1,23 +1,25 @@
-//package id.ac.ui.cs.advprog.account.controller;
-//import id.ac.ui.cs.advprog.account.service.AccountService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.RestController;
-//import java.util.Map;
-//import id.ac.ui.cs.advprog.authjwt.model.User;
-//
-//@RestController
-//@RequestMapping("/api/account")
-//public class AccountController {
-//    private final AccountService accountService;
-//
-//    public AccountController(AccountService accountService) {
-//        this.accountService = accountService;
-//    }
-//
-//    @PostMapping("/admin/delete")
-//    public ResponseEntity<Map<String, String>> delete(@RequestBody User user) {
-//        return accountService.delete(user.getUsername());
-//    }
-//}
+package id.ac.ui.cs.advprog.account.controller;
+import id.ac.ui.cs.advprog.account.dto.delete.DeleteRequestDTO;
+import id.ac.ui.cs.advprog.account.dto.delete.DeleteResponseDTO;
+import id.ac.ui.cs.advprog.account.service.AccountService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/account")
+public class AccountController {
+    public static final String USER = "/admin/user";
+
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+    @DeleteMapping(USER)
+    public ResponseEntity<DeleteResponseDTO> delete(@Valid @RequestBody DeleteRequestDTO email) {
+        return accountService.delete(email);
+    }
+}
