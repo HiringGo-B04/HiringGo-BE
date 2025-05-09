51a53793fad195b7efa67693c954b27dc7f7e9c6
DaoistXuandu <akbarraihan2005@gmail.com>
2025-05-09 13:08:41 +0700

    [GREEN] make impl for controller student

--- src/main/java/id/ac/ui/cs/advprog/authjwt/controller/AuthController.java
+++ src/main/java/id/ac/ui/cs/advprog/authjwt/controller/AuthController.java
@@ -2,6 +2,7 @@ package id.ac.ui.cs.advprog.authjwt.controller;
 import id.ac.ui.cs.advprog.authjwt.dto.AdminRegistrationDTO;
 import id.ac.ui.cs.advprog.authjwt.dto.LecturerRegistrationDTO;
 import id.ac.ui.cs.advprog.authjwt.dto.RegisterResponseDTO;
+import id.ac.ui.cs.advprog.authjwt.dto.StudentRegistrationDTO;
 import id.ac.ui.cs.advprog.authjwt.facade.AuthenticationFacade;
 import id.ac.ui.cs.advprog.authjwt.model.Token;
 import id.ac.ui.cs.advprog.authjwt.model.User;
@@ -27,12 +28,12 @@ public class AuthController {
     public ResponseEntity<RegisterResponseDTO> registerAdmin(@Valid @RequestBody AdminRegistrationDTO user) {
         return authFacade.register(user, "ADMIN");
     }
-//
-//    @PostMapping("/public/signup/student")
-//    public ResponseEntity<Map<String, String>> registerStudent(@RequestBody User user) {
-//        return authFacade.register(user, "STUDENT");
-//    }
-//
+
+    @PostMapping("/public/signup/student")
+    public ResponseEntity<RegisterResponseDTO> registerStudent(@Valid @RequestBody StudentRegistrationDTO user) {
+        return authFacade.register(user, "STUDENT");
+    }
+
     @PostMapping("/admin/signup/lecturer")
     public ResponseEntity<RegisterResponseDTO> registerLecturer(@Valid @RequestBody LecturerRegistrationDTO user) {
         return authFacade.register(user, "LECTURER");
