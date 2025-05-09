895e1c34e155640201b0f023f210ab8ad209f0f5
DaoistXuandu <akbarraihan2005@gmail.com>
2025-05-09 10:32:35 +0700

    [GREEN] add impl for lecture register

--- src/main/java/id/ac/ui/cs/advprog/authjwt/service/command/LecturerRegistrationCommand.java
+++ src/main/java/id/ac/ui/cs/advprog/authjwt/service/command/LecturerRegistrationCommand.java
@@ -1,77 +1,66 @@
-//package id.ac.ui.cs.advprog.authjwt.service.command;
-//
-//import id.ac.ui.cs.advprog.authjwt.model.User;
-//import id.ac.ui.cs.advprog.authjwt.repository.UserRepository;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//import java.util.HashMap;
-//import java.util.Map;
-//import java.util.UUID;
-//
-//public class LecturerRegistrationCommand extends RegistrationCommand {
-//    public LecturerRegistrationCommand(UserRepository userRepository, PasswordEncoder passwordEncoder, User user) {
-//        super(userRepository,passwordEncoder,user);
-//    }
-//
-//    @Override
-//    @Transactional
-//    public ResponseEntity<Map<String, String>> addUser() {
-//        Map<String, String> response = new HashMap<>();
-//        Map<String, String> validity = check_invalid_input("lecturer");
-//
-//        if(user.getPassword() == null
-//                || user.getUsername() == null
-//                || user.getFullName() == null
-//                || user.getNip() == null
-//                || user.getPassword().isEmpty()
-//                || user.getUsername().isEmpty()
-//                || user.getFullName().isEmpty()
-//                || user.getNip().isEmpty()
-//        ) {
-//            response.put("status", "error");
-//            response.put("message", "Invalid payload");
-//            return new ResponseEntity<>(response, HttpStatus.valueOf(403));
-//        }
-//
-//        if(!"valid".equals(validity.get("message"))) {
-//            response.put("status", "error");
-//            response.put("message", validity.get("message"));
-//            return new ResponseEntity<>(response, HttpStatus.valueOf(Integer.parseInt(validity.get("code"))));
-//        }
-//
-//        if(userRepository.existsByNip(user.getNip())) {
-//            response.put("status", "error");
-//            response.put("message", "Nip already exists");
-//            return new ResponseEntity<>(response, HttpStatus.valueOf(404));
-//        }
-//
-//        try{
-//            User newUser = new User(
-//                    UUID.randomUUID(),
-//                    user.getUsername(),
-//                    passwordEncoder.encode(user.getPassword()),
-//                    user.getFullName(),
-//                    true,
-//                    user.getNip()
-//            );
-//
-//            userRepository.save(newUser);
-//
-//            response.put("status", "accept");
-//            response.put("messages", "Success register");
-//            response.put("username", newUser.getUsername());
-//            response.put("role", "LECTURER");
-//
-//            return new ResponseEntity<>(response, HttpStatus.valueOf(200));
-//        }
-//        catch (Exception e) {
-//            response.put("status", "error");
-//            response.put("messages", e.getMessage());
-//            return new ResponseEntity<>(response, HttpStatus.valueOf(401));
-//        }
-//    }
-//
-//}
+package id.ac.ui.cs.advprog.authjwt.service.command;
+
+import id.ac.ui.cs.advprog.authjwt.dto.LecturerRegistrationDTO;
+import id.ac.ui.cs.advprog.authjwt.dto.RegisterResponseDTO;
+import id.ac.ui.cs.advprog.authjwt.dto.StudentRegistrationDTO;
+import id.ac.ui.cs.advprog.authjwt.model.User;
+import id.ac.ui.cs.advprog.authjwt.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Map;
+import java.util.UUID;
+
+public class LecturerRegistrationCommand extends RegistrationCommand {
+    public LecturerRegistrationCommand(UserRepository userRepository, PasswordEncoder passwordEncoder, LecturerRegistrationDTO user) {
+        super(userRepository,passwordEncoder,user);
+    }
+
+    @Override
+    @Transactional
+    public ResponseEntity<RegisterResponseDTO> addUser() {
+        LecturerRegistrationDTO lecturer = (LecturerRegistrationDTO) user;
+
+        Map<String, String> validity = check_invalid_input("lecturer");
+        if(!"valid".equals(validity.get("message"))) {
+            return new ResponseEntity<>(
+                    new RegisterResponseDTO("error", validity.get("message")),
+                    HttpStatus.valueOf(Integer.parseInt(validity.get("code"))));
+        }
+
+        if(userRepository.existsByNip(lecturer.nip())) {
+            return new ResponseEntity<>(
+                    new RegisterResponseDTO("error", "NIP Already exists"),
+                    HttpStatus.valueOf(400));
+        }
+
+        try{
+            User newUser = new User(
+                    UUID.randomUUID(),
+                    user.username(),
+                    passwordEncoder.encode(user.password()),
+                    ((LecturerRegistrationDTO) user).fullName(),
+                    true,
+                    ((LecturerRegistrationDTO) user).nip()
+            );
+
+            userRepository.save(newUser);
+
+            return new ResponseEntity<>(
+                    new RegisterResponseDTO(
+                            "accept",
+                            "Success register",
+                            newUser.getUsername(),
+                            "LECTURER"),
+                    HttpStatus.valueOf(200));
+        }
+        catch (Exception e) {
+            return new ResponseEntity<>(
+                    new RegisterResponseDTO("error", e.getMessage()),
+                    HttpStatus.valueOf(400));
+        }
+    }
+
+}
--- src/main/java/id/ac/ui/cs/advprog/authjwt/service/command/RegistrationCommand.java
+++ src/main/java/id/ac/ui/cs/advprog/authjwt/service/command/RegistrationCommand.java
@@ -1,8 +1,6 @@
 package id.ac.ui.cs.advprog.authjwt.service.command;
 import id.ac.ui.cs.advprog.authjwt.config.GeneralUtils;
-import id.ac.ui.cs.advprog.authjwt.dto.AdminRegistrationDTO;
-import id.ac.ui.cs.advprog.authjwt.dto.RegisterResponseDTO;
-import id.ac.ui.cs.advprog.authjwt.dto.UserDTO;
+import id.ac.ui.cs.advprog.authjwt.dto.*;
 import id.ac.ui.cs.advprog.authjwt.model.User;
 import id.ac.ui.cs.advprog.authjwt.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
@@ -19,29 +17,40 @@ public abstract class RegistrationCommand {
     public Map<String, String> check_invalid_input(String status) {
         Map<String, String> response = new HashMap<>();
         if(userRepository.existsByUsername(user.username())) {
-            response.put("code", "403");
+            response.put("code", "400");
             response.put("message", "Username already exists");
             return response;
         }
 
         if(!GeneralUtils.isValidEmail(user.username())) {
-            response.put("code", "403");
+            response.put("code", "400");
             response.put("message", "Username must be a valid email address");
             return response;
         }
 
         if(!status.equals("admin")){
-//            if(!GeneralUtils.isValidInt((status.equals("student")) ? user.nim() : user.nip())) {
-//                response.put("code", "403");
-//                response.put("message", "NIM/NIP must only contain number and maximal 12 digits long");
-//                return response;
-//            }
-//
-//            if(!GeneralUtils.isValidString(user.fullName())) {
-//                response.put("code", "403");
-//                response.put("message", "Name must only contain letter character");
-//                return response;
-//            }
+            String idNumber = null;
+            String fullName = "";
+
+            if ("student".equals(status) && user instanceof StudentRegistrationDTO student) {
+                idNumber = student.nim();
+                fullName = student.fullName();
+            } else if ("lecturer".equals(status) && user instanceof LecturerRegistrationDTO lecturer) {
+                idNumber = lecturer.nip();
+                fullName = lecturer.fullName();
+            }
+
+            if (!GeneralUtils.isValidInt(idNumber)) {
+                response.put("code", "400");
+                response.put("message", "NIM/NIP must only contain number and maximal 12 digits long");
+                return response;
+            }
+
+            if(!GeneralUtils.isValidString(fullName)) {
+                response.put("code", "400");
+                response.put("message", "Name must only contain letter character");
+                return response;
+            }
         }
 
         response.put("message", "valid");
