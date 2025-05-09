d103da2de48c82dcdf03fa29f81774a6973d3149
KoecengDaun <laurentius.farel@gmail.com>
2025-05-09 09:53:26 +0700

    [REFACTOR] request matcher (auth) to /**

--- src/main/java/id/ac/ui/cs/advprog/authjwt/config/SecurityConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/authjwt/config/SecurityConfig.java
@@ -18,7 +18,7 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/auth/**", "/", "/api/account/**").permitAll()
+                        .requestMatchers("/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
