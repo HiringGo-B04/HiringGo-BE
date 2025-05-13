fdab7a443ef375970b2b67244bd3a97264bdc062
DaoistXuandu <akbarraihan2005@gmail.com>
2025-05-14 00:49:44 +0700

    [RED] add test for abstract user

--- src/main/java/id/ac/ui/cs/advprog/account/dto/update/UserIntoAdminDTO.java
+++ src/main/java/id/ac/ui/cs/advprog/account/dto/update/UserIntoAdminDTO.java
@@ -1,12 +1,4 @@
 package id.ac.ui.cs.advprog.account.dto.update;
 
-
-import id.ac.ui.cs.advprog.account.dto.update.UserUpdateDTO;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-public record UserIntoAdminDTO (
-        @NotBlank
-        @NotNull
-        String role
-) implements UserUpdateDTO {}
+public class UserIntoAdminDTO extends UserUpdateDTO {
+}
