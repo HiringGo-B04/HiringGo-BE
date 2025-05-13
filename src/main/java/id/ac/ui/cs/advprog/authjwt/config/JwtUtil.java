a271adf74412e6dd473242cabaae48bd1a731183
DaoistXuandu <akbarraihan2005@gmail.com>
2025-05-14 01:19:43 +0700

    [GREEN] add impl for not found token

--- src/main/java/id/ac/ui/cs/advprog/authjwt/config/JwtUtil.java
+++ src/main/java/id/ac/ui/cs/advprog/authjwt/config/JwtUtil.java
@@ -45,6 +45,12 @@ public class JwtUtil {
     public boolean validateJwtToken(String token) {
         try {
             Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+
+            if(tokenRepository.findByToken(token) == null) {
+                System.out.println("Token not found");
+                return false;
+            }
+
             return true;
         } catch (Exception e) {
             System.out.println("JWT validation error: " + e.getMessage());
