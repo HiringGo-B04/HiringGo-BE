98cf4bd8f0dbc3560be9e1191b2cd7056d4bafb5
DaoistXuandu <akbarraihan2005@gmail.com>
2025-05-14 00:53:22 +0700

    [GREEN] add impl for abstract user

--- src/main/java/id/ac/ui/cs/advprog/account/dto/update/UserIntoAdminDTO.java
+++
@@ -1,4 +0,0 @@
-package id.ac.ui.cs.advprog.account.dto.update;
-
-public class UserIntoAdminDTO extends UserUpdateDTO {
-}
--- src/main/java/id/ac/ui/cs/advprog/account/dto/update/UserUpdateDTO.java
+++ src/main/java/id/ac/ui/cs/advprog/account/dto/update/UserUpdateDTO.java
@@ -1,6 +1,21 @@
 package id.ac.ui.cs.advprog.account.dto.update;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import jakarta.validation.constraints.NotBlank;
 
-public interface UserUpdateDTO {
-    String role();
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "role",
+        visible = true
+)
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = UserIntoAdminDTO.class, name = "ADMIN"),
+})
+public abstract class UserUpdateDTO {
+    @NotBlank
+    public String role;
+
+    @NotBlank
+    public String username;
 }
-
