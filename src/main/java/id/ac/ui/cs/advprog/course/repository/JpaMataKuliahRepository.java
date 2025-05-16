436d67cde7fae385fc2344ef714e6cf148301b91
KoecengDaun <laurentius.farel@gmail.com>
2025-05-16 14:44:10 +0700

    [REFACTOR] Update RepositoryJPA

--- src/main/java/id/ac/ui/cs/advprog/course/repository/JpaMataKuliahRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/course/repository/JpaMataKuliahRepository.java
@@ -3,16 +3,19 @@ package id.ac.ui.cs.advprog.course.repository;
 import id.ac.ui.cs.advprog.course.model.MataKuliah;
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Repository;
 
-/** CATATAN
- * Implementasi repository berbasis Spring Data JPA.
- * Aktif di semua profile <b>kecuali</b> "test" — sehingga unit‑test tetap memakai
- * {@link InMemoryMataKuliahRepository}.
- */
+
 @Repository
 @Profile({"!test","jpa-test"})
 public interface JpaMataKuliahRepository
         extends JpaRepository<MataKuliah, String>,
         MataKuliahRepository {
+
+    @Override
+    default void deleteByKode(@NonNull String kode) {
+        deleteById(kode);
+    }
 }
+
