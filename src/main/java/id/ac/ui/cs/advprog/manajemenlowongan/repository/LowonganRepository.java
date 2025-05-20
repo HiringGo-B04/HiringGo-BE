b999023cf36eb864e6891d5a62a5b7b6ef81fb9c
rakbidb <rakbidb@gmail.com>
2025-05-20 23:02:26 +0700

    [REFACTOR]: removed unused imports

--- src/main/java/id/ac/ui/cs/advprog/manajemenlowongan/repository/LowonganRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/manajemenlowongan/repository/LowonganRepository.java
@@ -1,14 +1,9 @@
 package id.ac.ui.cs.advprog.manajemenlowongan.repository;
 
 import id.ac.ui.cs.advprog.manajemenlowongan.model.Lowongan;
-import id.ac.ui.cs.advprog.mendaftarlowongan.model.Lamaran;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Iterator;
 import java.util.UUID;
 
 @Repository
