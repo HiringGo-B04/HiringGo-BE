01ea06784d4abfce10f1470cf2feef4e04160385
rakbidb <rakbidb@gmail.com>
2025-05-21 11:11:02 +0700

    [REFACTOR]: remove unused import

--- src/main/java/id/ac/ui/cs/advprog/manajemenlowongan/service/LowonganServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/manajemenlowongan/service/LowonganServiceImpl.java
@@ -3,7 +3,6 @@ package id.ac.ui.cs.advprog.manajemenlowongan.service;
 import id.ac.ui.cs.advprog.manajemenlowongan.model.Lowongan;
 import id.ac.ui.cs.advprog.manajemenlowongan.repository.LowonganRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
