b66e10395269ff51d27ab5c0c8c2c7cb11d20891
MaulRai <muhammadraihanofc@gmail.com>
2025-05-21 21:43:41 +0700

    fix: authority scope for lecturer role in lowongan urls

--- src/main/java/id/ac/ui/cs/advprog/authjwt/config/SecurityConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/authjwt/config/SecurityConfig.java
@@ -45,13 +45,14 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/user/**").hasAnyRole("ADMIN", "STUDENT", "LECTURER")
 
                         .requestMatchers("/api/account/admin/**").hasRole("ADMIN")
+
                         .requestMatchers("/api/lamaran/student/**").hasRole("STUDENT")
                         .requestMatchers("/api/lamaran/user/**").hasAnyRole("ADMIN", "STUDENT", "LECTURER")
                         .requestMatchers("/api/lamaran/lecturer/**").hasRole("LECTURER")
 
                         // for debugging purpose
                         .requestMatchers("/api/lowongan/user/**").hasAnyRole("ADMIN", "STUDENT", "LECTURER")
-                        .requestMatchers("/api/lowongan/all/user/**").hasAnyRole("ADMIN", "STUDENT", "LECTURER")
+                        .requestMatchers("/api/lowongan/lecturer/**").hasRole("LECTURER")
                         /*
                          * Hingga sini yang permitAll bawah ngga usah itu kayak namanya nge permit semuanya
                          */
