4698599ee3614870db83c533d000b48c5c4c9641
DaoistXuandu <akbarraihan2005@gmail.com>
2025-05-22 13:27:54 +0700

    [DEPLOY] deploy without log

--- src/main/java/id/ac/ui/cs/advprog/HiringGoApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/HiringGoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = {
         "id.ac.ui.cs.advprog.course",
-        "id.ac.ui.cs.advprog.log",
         "id.ac.ui.cs.advprog.manajemenlowongan",
         "id.ac.ui.cs.advprog.mendaftarlowongan",
         "id.ac.ui.cs.advprog.authjwt",
