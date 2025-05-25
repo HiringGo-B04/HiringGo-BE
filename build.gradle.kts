38e981c1cc08a7d1c17d73eb13978a5ad99fdbc1
rakbidb <rakbidb@gmail.com>
2025-05-25 19:37:17 +0700

    changed sonarqube version

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("org.springframework.boot") version "3.4.4"
     id("io.spring.dependency-management") version "1.1.7"
     id("com.github.johnrengelman.processes") version "0.5.0"
-    id("org.sonarqube") version "4.4.1.3373" // or latest version
+    id("org.sonarqube") version "3.5.0.2730" // or latest version
 }
 
 group = "id.ac.ui.cs.advprog"
