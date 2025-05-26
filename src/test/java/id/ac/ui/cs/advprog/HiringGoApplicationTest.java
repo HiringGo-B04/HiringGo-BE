a6fb3e3ed73a2b86ba892394554980434bb6ddbd
DaoistXuandu <akbarraihan2005@gmail.com>
2025-05-26 11:37:30 +0700

    Revert "miss"
    This reverts commit bd12deb6b64d11c3568a2bf7112de98c660e632e.

--- src/test/java/id/ac/ui/cs/advprog/HiringGoApplicationTest.java
+++ src/test/java/id/ac/ui/cs/advprog/HiringGoApplicationTest.java
@@ -2,14 +2,11 @@ package id.ac.ui.cs.advprog;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 
 class HiringGoApplicationTest {
 
     @Test
     void contextLoads() {
-        assertTrue(true);
     }
 
 }
