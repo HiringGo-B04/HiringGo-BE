bd12deb6b64d11c3568a2bf7112de98c660e632e
DaoistXuandu <akbarraihan2005@gmail.com>
2025-05-26 11:22:46 +0700

    miss

--- src/test/java/id/ac/ui/cs/advprog/HiringGoApplicationTest.java
+++ src/test/java/id/ac/ui/cs/advprog/HiringGoApplicationTest.java
@@ -2,11 +2,14 @@ package id.ac.ui.cs.advprog;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 class HiringGoApplicationTest {
 
     @Test
     void contextLoads() {
+        assertTrue(true);
     }
 
 }
